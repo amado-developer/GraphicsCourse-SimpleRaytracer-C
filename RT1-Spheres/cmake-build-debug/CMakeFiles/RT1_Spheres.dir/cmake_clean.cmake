8c3c023e4b6a58c076d75b8dce2cfe86e9547e98
Amado-Developer <amado_developer@hotmail.com>
2020-09-07 16:43:37 -0600

    starting the project

---
+++ RT1-Spheres/cmake-build-debug/CMakeFiles/RT1_Spheres.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RT1_Spheres.dir/main.cpp.o"
+  "RT1_Spheres"
+  "RT1_Spheres.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RT1_Spheres.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ RT1-Spheres/cmake-build-debug/CMakeFiles/RT1_Spheres.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for RT1_Spheres.
+# This may be replaced when dependencies are built.
